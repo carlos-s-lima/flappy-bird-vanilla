c908a7a861c0c5f2791ee799d8e505ca39228b18
Carlos Lima <carlosjrslima@gmail.com>
2025-10-15 16:00:04 -0300

    CREATED element constructor function at app.js CREATED barriers constructor function CREATED barriers animation ADD bird.png UPDATED barriers style UPDATED basic html and css structure

--- src/app.js
+++ src/app.js
@@ -1,1 +1,80 @@
-console.log("Hello World");
+// Função construtora de um elemento
+function novoElemento(tagName, className) {
+    const element = document.createElement(tagName)
+    element.className = className
+    return element
+}
+// Função construtora de uma barreira
+function Barreira(reversa = false) {
+    this.elemento = novoElemento("div", "barreira")
+
+    const bico = novoElemento("div", "bico")
+    const corpo = novoElemento("div", "corpo")
+
+    this.elemento.appendChild(reversa ? corpo : bico)
+    this.elemento.appendChild(reversa ? bico : corpo)
+
+    this.setAltura = altura => corpo.style.height = `${altura}px`
+}
+// Função construtora de um par de barreiras
+function parDeBarreiras(altura, abertura, x) {
+    this.elemento = novoElemento("div", "barreiras")
+
+    this.superior = new Barreira(true)
+    this.inferior = new Barreira(false)
+
+    this.elemento.appendChild(this.superior.elemento)
+    this.elemento.appendChild(this.inferior.elemento)
+
+    this.sortearAbertura = () => {
+        const alturaSuperior = Math.random() * (altura - abertura)
+        const alturaInferior = altura - abertura - alturaSuperior
+        this.superior.setAltura(alturaSuperior)
+        this.inferior.setAltura(alturaInferior)
+    }
+
+    this.getX = () => parseInt(this.elemento.style.left.split("px")[0])
+    
+    this.setX = x => this.elemento.style.left = `${x}px`
+
+    this.getLargura = () => this.elemento.clientWidth
+
+    this.sortearAbertura()
+    this.setX(x)
+}
+
+// Função responsável por criar, animar e reaproveitar as barreiras do jogo //
+function Esteira(altura, largura, abertura, espaco, notificarPonto) {
+    this.pares = [
+        new parDeBarreiras(altura, abertura, largura),
+        new parDeBarreiras(altura, abertura, largura + espaco),
+        new parDeBarreiras(altura, abertura, largura + 2 * espaco),
+        new parDeBarreiras(altura, abertura, largura + 3 * espaco),
+        new parDeBarreiras(altura, abertura, largura + 4 * espaco)
+    ]
+    
+    const deslocamento = 3
+    this.animar = () => {
+        this.pares.forEach(par => {
+            par.setX(par.getX() - deslocamento)
+
+            if (par.getX() < -par.getLargura()) {
+                par.setX(par.getX() + espaco * this.pares.length)
+                par.sortearAbertura()
+            }
+        
+        const meio = largura / 2
+        const cruzouOMeio = par.getX() + deslocamento >= meio && par.getX() < meio
+        if(cruzouOMeio) notificarPonto()
+
+        })
+        }
+    }
+
+    const esteira = new Esteira(700, 1000, 300, 400)
+    const areaDoJogo = document.querySelector(".gameContainer")
+    esteira.pares.forEach(par => areaDoJogo.appendChild(par.elemento))
+
+    setInterval(() => {
+        esteira.animar()
+    }, 20)
